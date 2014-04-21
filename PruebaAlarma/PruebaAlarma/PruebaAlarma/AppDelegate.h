2edcbfd2853fed9b51fb4b2a2672c151f67f22a9
witcharrow <alejandro.marijuan@googlemail.com>
2014-04-21 23:23:36 +0100

    Alarmar con alertas funcionando. Falta control idioma para datepicker.

---
+++ PruebaAlarma/PruebaAlarma/PruebaAlarma/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PruebaAlarma
+//
+//  Created by user11394 on 21/04/14.
+//  Copyright (c) 2014 Alejandro Marijuán. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
